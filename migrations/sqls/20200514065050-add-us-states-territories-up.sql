6bf1794a5d526a3b857dbfcc0a8e203244db65bd
Greg Orzell <gorzell@github.com>
2020-05-14 08:58:25 +0200

    regions: Add missing US states and territories.
    Add US states that we had removed from the UI back in as well as the US
    territories.

---
+++ migrations/sqls/20200514065050-add-us-states-territories-up.sql
@@ -0,0 +1,5 @@
+INSERT INTO regions (id, parent_id, name, created_at, updated_at) VALUES
+  ('US-AS', 'US', 'American Samoa', NOW(), NOW()),
+  ('US-GU', 'US', 'Guam', NOW(), NOW()),
+  ('US-PR', 'US', 'Puerto Rico', NOW(), NOW()),
+  ('US-VI', 'US', 'US Virgin Islands', NOW(), NOW());
